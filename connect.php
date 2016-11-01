53401eeff9841b08c2d3f1c703adb2e4d82cc1e0
VIGUIER Julien <viguierjulien@mbp-de-viguier.wireless.concordia.ca>
2016-11-01 10:04:37 -0400

    Description comments

---
+++ connect.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Page de connexion</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="font.css">
+</head>
+<body>
+	<div class="header">
+
+		<a href="mainpage.html">
+			<img src="Logo.png">
+		</a>
+		
+		<div class="searchbar">
+			<form action="search.php" method="post">
+				<input type="text" name="search" placeholder="Search for a product, a brand, ...">
+				<input type="submit" value="Search">
+			</form>
+		</div>
+		
+		<div class="connexion">
+			<a href="connect.php">
+				Connexion
+			</a>
+		</div>
+
+		<div class="panier">
+			<a href="basket.php">
+				Basket
+			</a>
+		</div>
+	
+	</div>
+
+</body>
+</html>
